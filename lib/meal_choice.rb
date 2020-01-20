8d6585920b478bad21815cd58d5794ab67ac5a61
Yahya  Gok <ygok17@hotmail.com>
2020-01-20 15:30:37 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -18,11 +18,11 @@ meal_choice(protein, veg1, veg2)
 #   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 # end
 # meal_choice(protein, veg1, veg2)
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
 # def meal_choice(protein, veg1, veg2)
 #   puts "What a nutritious meal!"
 #   puts "A plate of #{protein} with #{veg1} and #{veg2}."
