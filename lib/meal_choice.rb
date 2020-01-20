a555ae69a8f83d78ebe14d0688c2a0b35abb4b81
Yahya  Gok <ygok17@hotmail.com>
2020-01-20 15:17:20 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,4 +7,6 @@ def meal_choice(veg1,veg2,protein= "meat")
   return puts "A plate of #{protein} with #{veg1} and #{veg2}."
 
 end
- meal_choice(veg1,veg2,protein)
+
+
+meal_choice(veg1,veg2,protein)
